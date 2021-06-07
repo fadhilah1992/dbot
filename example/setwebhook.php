beadc2b889a8417b05f23bda467c40dda0e36376
Fathurrohman <80453312+fathurrohman26@users.noreply.github.com>
2021-06-07 15:00:10 +0700

    Update setwebhook.php

--- example/setwebhook.php
+++ example/setwebhook.php
@@ -7,15 +7,11 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $bot = new Dbot('BOT-TOKEN');
 
 
-$bot->setWebhook('https://myhost.example/handle_bot.php');
+$bot->setWebhook('url-webhook');
 var_dump($bot->getWebhookInfo());
 
 // custom certificate file
-$bot->setWebhook('https://myhost.example/handle_bot.php', __DIR__ . '/cert.pem');
-var_dump($bot->getWebhookInfo());
-
-// delete webhook
-echo "delete webhook";
-$bot->deleteWebhook(true);
-var_dump($bot->getWebhookInfo());
-
+/**
+ * $bot->setWebhook('url-webhook', __DIR__ . '/cert.pem');
+ * var_dump($bot->getWebhookInfo());
+ */
